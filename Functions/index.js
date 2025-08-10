43f37ebc4b4a57237b4a5a2ba56481e628e93461
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-08-10 23:18:17 +0530

    add proper comments throughout

--- Functions/index.js
+++ Functions/index.js
@@ -9,24 +9,21 @@
 /* 1) Function declaration...
 Function declaration is where you define the function with a function keyword, followed by function name...
 something like this...
-
-
-function sayHello() {
-console.log(`Hello!...`);
-}
-
-Now this function can be invoked anywhere you want in the current JS file...
-Also remember that you can call this function anywhere in this file because of hoisting...
-sayHello();
-
-********Important***********
-
-Function declarations are hoisted...
 */
 
-// As in other languages like C,C++, and Java, we can also pass arguments to the functions...
+function sayHello() {
+  console.log(`Hello!...`);
+}
 
-/* 
+// Now this function can be invoked anywhere you want in the current JS file...
+// Also remember that you can call this function anywhere in this file because of hoisting...
+sayHello();
+
+// ********Important***********
+
+// Function declarations are hoisted...
+
+// As in other languages like C,C++, and Java, we can also pass arguments to the functions...
 
 // name here is the parameter that is holding the passed argument's value...
 function sayHello(name) {
@@ -37,11 +34,7 @@ sayHello("Shaill"); // here Shaill is an argument...
 sayHello("Shailesh");
 sayHello("Riyansh");
 
-*/
-
-/*
-2) Function expression is where a variable holds a function definition... 
-
+// 2) Function expression is where a variable holds a function definition...
 
 const greet = function () {
   console.log(`Hello from function expression`);
@@ -50,11 +43,10 @@ const greet = function () {
 console.log(greet);
 console.log(greet());
 
-*/
-
 // 3) Callback functions...
 
 /* Definition : Callback functions are those functions which are passed as an argument to another function and that function invokes the passed function either immediately or after executing its own logic or on any event occurs...
+ */
 
 function doHomework(subject, fn) {
   console.log(`Started doing my ${subject} homework`);
@@ -67,27 +59,21 @@ function completedHomework() {
 
 doHomework("Maths", completedHomework);
 
-*/
-
 // 4) Anonymous functions...
 
 /* Anonymous function is a function which doesnt have any name defined for it!...
 This kind of function aren't reusable and can be just used once...
-for example : 
+*/
+// for example :
 
 setTimeout(function () {
   console.log(`Hello shaill`);
 }, 3000);
 
-
-*/
-
 // 5) Arrow function...
-/*
-Arrow function is a shorter syntax for traditional function where we dont have access to 'this' keyword...
 
-for example:
-
+/*Arrow function is a shorter syntax for traditional function where we dont have access to 'this' keyword.*/
+// for example:
 const Header = () => {
   console.log(`Hello from arrow function`);
 };
@@ -109,68 +95,10 @@ const sayHii = (name = "Riyansh") => {
 sayHii(); // executes the function with default parameter...
 sayHii("Shailesh"); // executes the function with this argument...
 
-*/
-
 // 6) Immediately invoked function expression (IIFE)...
-/*
-Immediately invoked function expression gets executed as soon as its defined...
-for example:
+// Immediately invoked function expression gets executed as soon as its defined...
+// for example:
 
 (function sayBye() {
   console.log(`Bye bye!...`);
 })();
-
-this function doesn't need to be invoked manually as it's invoked the moment we defined it using the paranthesis.
-
-*/
-
-// const names = ["shailesh", "megha", "nilesh"];
-
-// console.log(names.length);
-
-// names.forEach((currEle, index) => {
-//   console.log(`My name is ${currEle + " and I'm a good boy"}`);
-// });
-
-// for (let i = 0; i < names.length; i++) {
-//   console.log(names[i]);
-// }
-
-// console.log(`Original array is:`, names);
-
-// let numbers = [19, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// numbers.forEach((currEle, index) => {
-//   if (currEle === 2) {
-//     console.log(`2 is current element`);
-//     break;
-//   } else {
-//     console.log(currEle);
-//   }
-// });
-
-// numbers.push(11);
-
-// console.log(numbers);
-
-// const sortednumbers = numbers.sort();
-// console.log(sortednumbers);
-
-// const newObj = {
-//   name: "Shailesh",
-//   // age: 22,
-//   gender: "Male",
-//   salary: 28000,
-// };
-
-// const { name, salary } = newObj;
-
-// console.log(newObj.age);
-// console.log(`Name of the employee is ${name} and salary is ${salary}`);
-
-// const arr = [10, 20, 30, 40];
-
-// // console.log(`Array element are: `, ...arr);
-// const destructuredArray = [...arr, 50];
-// console.log(destructuredArray);
-// console.log(typeof destructuredArray);
